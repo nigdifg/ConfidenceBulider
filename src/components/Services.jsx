39d495bd1fa885786d22658885f866e3a8cfb53e
Anurag <2006535@kiit.ac.in>
2024-02-29 17:05:43 +0530

    added faq

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -5,7 +5,10 @@ import React from 'react';
 function Services() {
 
   return (
+    <>
+   
       <div>
+
     <div className="pt-20 bg-gray-800">
   <div className="header">
     <h1 className="text-4xl font-extrabold text-center text-black dark:text-white">We've got you <span className="text-tmk-blue">covered.</span></h1>
@@ -137,8 +140,163 @@ function Services() {
             </div>
           </div>
         </div>
-      
+
+        
+
       </div>
+      <section className="py-24">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div
+          className="flex flex-col justify-center items-center gap-x-16 gap-y-5 xl:gap-28 lg:flex-row lg:justify-between max-lg:max-w-2xl mx-auto max-w-full"
+        >
+          <div className="w-full lg:w-1/2">
+            <img
+              src="https://pagedone.io/asset/uploads/1696230182.png"
+              alt="FAQ tailwind section"
+              className="w-full"
+            />
+          </div>
+          <div className="w-full lg:w-1/2">
+            <div className="lg:max-w-xl">
+              <div className="mb-6 lg:mb-16">
+                <h6
+                  className="text-2xl text-center font-medium text-blue-600 mb-2 lg:text-left"
+                >
+                  Frequently Asked Questions
+                </h6>
+                <h2
+                  className="text-4xl text-center font-bold text-gray-300 leading-[3.25rem] mb-5 lg:text-left"
+                >
+                  Looking for answers?
+                </h2>
+              </div>
+              <div className="accordion-group" data-accordion="default-accordion">
+                <div
+                  className="accordion pb-8 border-b border-solid border-gray-200 active"
+                  id="basic-heading-one-with-arrow-always-open"
+                >
+                  <button
+                    className="accordion-toggle group inline-flex items-center justify-between text-xl font-normal leading-8 text-blue-600 w-full transition duration-500 hover:text-blue-600 accordion-active:text-indigo-600 accordion-active:font-medium always-open"
+                    aria-controls="basic-collapse-one-with-arrow-always-open"
+                  >
+                    <h5 >How to Book Slot?</h5>
+                    <svg
+                      className="text-gray-900 transition duration-500 group-hover:text-blue-600 accordion-active:text-indigo-600 accordion-active:rotate-180"
+                      width="22"
+                      height="22"
+                      viewBox="0 0 22 22"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M16.5 8.25L12.4142 12.3358C11.7475 13.0025 11.4142 13.3358 11 13.3358C10.5858 13.3358 10.2525 13.0025 9.58579 12.3358L5.5 8.25"
+                        stroke="currentColor"
+                        stroke-width="1.6"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      ></path>
+                    </svg>
+                  </button>
+                  <div
+                    id="basic-collapse-one-with-arrow-always-open"
+                    className="accordion-content w-full px-0 overflow-hidden pr-4 active"
+                    aria-labelledby="basic-heading-one-with-arrow-always-open"
+                  >
+                    <p className="text-base font-normal text-gray-300 ">
+                      To book a slot, you can visit our website and click on the
+                      "Book Slot" button.
+                    </p>
+                  </div>
+                </div>
+                <div
+                  className="accordion py-8 border-b border-solid border-gray-200 "
+                  id="basic-heading-two-with-arrow-always-open"
+                >
+                  <button
+                    className="accordion-toggle group inline-flex items-center justify-between font-normal text-xl leading-8 text-blue-600 w-full transition duration-500 hover:text-blue-600 accordion-active:text-indigo-600 accordion-active:font-medium"
+                    aria-controls="basic-collapse-two-with-arrow-always-open"
+                  >
+                    <h5>Have any trust issue?</h5>
+                    <svg
+                      className="text-gray-900 transition duration-500 group-hover:text-blue-600 accordion-active:text-indigo-600 accordion-active:rotate-180"
+                      width="22"
+                      height="22"
+                      viewBox="0 0 22 22"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M16.5 8.25L12.4142 12.3358C11.7475 13.0025 11.4142 13.3358 11 13.3358C10.5858 13.3358 10.2525 13.0025 9.58579 12.3358L5.5 8.25"
+                        stroke="currentColor"
+                        stroke-width="1.6"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      ></path>
+                    </svg>
+                  </button>
+                  <div
+                    id="basic-collapse-two-with-arrow-always-open"
+                    className="accordion-content w-full px-0 overflow-hidden pr-4"
+                    aria-labelledby="basic-heading-two-with-arrow-always-open"
+                    
+                  >
+                    <p className="text-base text-gray-300 font-normal">
+                      Our focus on providing robust and user-friendly content
+                       apabilities ensures that you can prepare for your
+                      interview with confidence, and achieve your dream job.
+                    </p>
+                  </div>
+                </div>
+                <div
+                  className="accordion py-8 border-b border-solid border-gray-200"
+                  id="basic-heading-three-with-arrow-always-open"
+                >
+                  <button
+                    className="accordion-toggle group inline-flex items-center justify-between text-xl font-normal leading-8 text-blue-600 w-full transition duration-500 hover:text-indigo-600 accordion-active:font-medium accordion-active:text-indigo-600"
+                    aria-controls="basic-collapse-three-with-arrow-always-open"
+                  >
+                    <h5>How much it cost?</h5>
+                    <svg
+                      className="text-gray-900 transition duration-500 group-hover:text-blue-600 accordion-active:text-indigo-600 accordion-active:rotate-180"
+                      width="22"
+                      height="22"
+                      viewBox="0 0 22 22"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M16.5 8.25L12.4142 12.3358C11.7475 13.0025 11.4142 13.3358 11 13.3358C10.5858 13.3358 10.2525 13.0025 9.58579 12.3358L5.5 8.25"
+                        stroke="currentColor"
+                        stroke-width="1.6"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      ></path>
+                    </svg>
+                  </button>
+                  <div
+                    id="basic-collapse-three-with-arrow-always-open"
+                    className="accordion-content w-full px-0 overflow-hidden pr-4"
+                    aria-labelledby="basic-heading-three-with-arrow-always-open"
+                  >
+                    <p className="text-base text-gray-300 font-normal">
+                      Our focus is on providing best services at affordable
+                      prices. We have different plans for different services and
+                      you can choose according to your need.
+                      But we assure you that you will get the best services at
+                      affordable prices.
+                    </p>
+                  </div>
+                </div>
+               
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+                                            
+                                 
+      </>
   );
 }
 
